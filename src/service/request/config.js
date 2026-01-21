d98d0014fdbc29bd00fd9ef0b4fd298d5dee5dbb
cjf717 <cjf54@163.com>
2026-01-21 15:05:17 +0800

    删除多余js文件

--- src/service/request/config.js
+++
@@ -1,5 +0,0 @@
-// service/request/config.ts
-const BASE_URL = "https://vercel-netease-cloud-music-api-enhanced.gh9527.de5.net/";
-// const BASE_URL = "https://https://vercel-netease-cloud-music-api-enhanced.git9527.de5.net/";
-const TIME_OUT = 10000;
-export { BASE_URL, TIME_OUT };
--- src/utils/formatTime.js
+++
@@ -1,7 +0,0 @@
-import dayjs from "dayjs";
-export function formatTime(time, format = "YYYY-MM-DD HH:mm:ss") {
-    if (typeof time === "string") {
-        time = Number(time);
-    }
-    return dayjs(time).format(format);
-}
